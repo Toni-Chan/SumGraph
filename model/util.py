5700a833e813054bf69549381cbf0b711a4f89b9
Toni-Chan <chens943366483@gmail.com>
2020-11-23 00:32:36 -0500

    Multibart + GAT basic

---
+++ model/util.py
@@ -0,0 +1,165 @@
+'''
+Author: your name
+Date: 1969-12-31 19:00:00
+LastEditTime: 2020-11-18 03:59:23
+LastEditors: Please set LastEditors
+Description: In User Settings Edit
+FilePath: /Capstone/src/model/util.py
+'''
+import math
+
+import torch
+from torch.nn import functional as F
+from torch import Tensor, nn
+import numpy as np
+
+
+#################### general sequence helper #########################
+def len_mask(lens, device, max_num=None):
+    """ users are resposible for shaping
+    Return: tensor_type [B, T]
+    """
+    if max_num is not None:
+        max_len = max_num
+    else:
+        max_len = max(lens)
+    batch_size = len(lens)
+    mask = torch.ByteTensor(batch_size, max_len).to(device)
+    mask.fill_(0)
+    for i, l in enumerate(lens):
+        mask[i, :l].fill_(1)
+    return mask
+
+
+def sequence_mean(sequence, seq_lens, dim=1):
+    if seq_lens:
+        assert sequence.size(0) == len(seq_lens)   # batch_size
+        sum_ = torch.sum(sequence, dim=dim, keepdim=False)
+        mean = torch.stack([s/l for s, l in zip(sum_, seq_lens)], dim=0)
+    else:
+        mean = torch.mean(sequence, dim=dim, keepdim=False)
+    return mean
+
+def sequence_loss(logits, targets, xent_fn=None, pad_idx=0, if_aux=False, fp16=False):
+    """ functional interface of SequenceLoss"""
+    if if_aux:
+        assert logits.size() == targets.size()
+    else:
+        assert logits.size()[:-1] == targets.size()
+
+    mask = targets != pad_idx
+    target = targets.masked_select(mask)
+    if if_aux:
+        target = target.float()
+        logit = logits.masked_select(
+            mask
+        ).contiguous()
+    else:
+        logit = logits.masked_select(
+            mask.unsqueeze(2).expand_as(logits)
+        ).contiguous().view(-1, logits.size(-1))
+    if xent_fn:
+        if fp16:
+            logit = torch.log(logit/(1-logit))
+        loss = xent_fn(logit, target)
+    else:
+        loss = F.cross_entropy(logit, target)
+    assert (not math.isnan(loss.mean().item())
+            and not math.isinf(loss.mean().item()))
+    return loss
+
+def binary_sequence_loss(logits, targets, xent_fn=None, pad_idx=0):
+    """ functional interface of SequenceLoss"""
+    # print('target:', targets.size())
+    # print('logit:', logits.size())
+    assert logits.size() == targets.size()
+
+    # print('target:', targets)
+    # print('logit:', logits)
+    mask = targets != pad_idx
+    target = targets.masked_select(mask)
+    logit = logits.masked_select(
+        mask
+    )
+    # print('target:', target)
+    # print('logit:', logit)
+    if xent_fn:
+        loss = xent_fn(logit, target)
+    else:
+        loss = F.cross_entropy(logit, target)
+    assert (not math.isnan(loss.mean().item())
+            and not math.isinf(loss.mean().item()))
+    return loss
+
+#################### bart helpers ####################################
+def invert_mask(attention_mask):
+    """Turns 1->0, 0->1, False->True, True-> False"""
+    assert attention_mask.dim() == 2
+    return attention_mask.eq(0)
+
+def _prepare_bart_decoder_inputs(
+    config, input_ids, decoder_input_ids=None, decoder_padding_mask=None, causal_mask_dtype=torch.float32
+):
+    """
+    Prepare masks that ignore padding tokens in the decoder and a causal mask for the decoder if none are provided.
+    This mimics the default behavior in fairseq. To override it pass in masks. Note: this is not called during
+    generation
+    """
+    pad_token_id = config.pad_token_id
+    if decoder_input_ids is None:
+        decoder_input_ids = shift_tokens_right(input_ids, pad_token_id)
+    bsz, tgt_len = decoder_input_ids.size()
+    if decoder_padding_mask is None:
+        decoder_padding_mask = make_padding_mask(decoder_input_ids, pad_token_id)
+    else:
+        decoder_padding_mask = invert_mask(decoder_padding_mask)
+    if decoder_padding_mask is not None and decoder_padding_mask.shape[1] > 1:
+        # never mask leading token, even if it is pad
+        decoder_padding_mask[:, 0] = decoder_padding_mask[:, 1]
+    tmp = fill_with_neg_inf(torch.zeros(tgt_len, tgt_len))
+    mask = torch.arange(tmp.size(-1))
+    tmp.masked_fill_(mask < (mask + 1).view(tmp.size(-1), 1), 0)
+    causal_mask = tmp.to(dtype=causal_mask_dtype, device=decoder_input_ids.device)
+    return decoder_input_ids, decoder_padding_mask, causal_mask
+
+def _make_linear_from_emb(emb):
+    vocab_size, emb_size = emb.weight.shape
+    lin_layer = nn.Linear(vocab_size, emb_size, bias=False)
+    lin_layer.weight.data = emb.weight.data
+    return lin_layer
+
+def shift_tokens_right(input_ids, pad_token_id):
+    """Shift input ids one token to the right, and wrap the last non pad token (usually <eos>)."""
+    prev_output_tokens = input_ids.clone()
+    index_of_eos = (input_ids.ne(pad_token_id).sum(dim=1) - 1).unsqueeze(-1)
+    prev_output_tokens[:, 0] = input_ids.gather(1, index_of_eos).squeeze()
+    prev_output_tokens[:, 1:] = input_ids[:, :-1]
+    return prev_output_tokens
+
+def make_padding_mask(input_ids, padding_idx=1):
+    """True for pad tokens"""
+    padding_mask = input_ids.eq(padding_idx)
+    if not padding_mask.any():
+        padding_mask = None
+    return padding_mask
+
+def _reorder_buffer(attn_cache: Dict, new_order) -> Dict:
+    for k, input_buffer_k in attn_cache.items():
+        if input_buffer_k is not None:
+            attn_cache[k] = input_buffer_k.index_select(0, new_order)
+    return attn_cache
+
+def LayerNorm(normalized_shape, eps=1e-5, elementwise_affine=True):
+    if torch.cuda.is_available():
+        try:
+            from apex.normalization import FusedLayerNorm
+
+            return FusedLayerNorm(normalized_shape, eps, elementwise_affine)
+        except ImportError:
+            pass
+    return torch.nn.LayerNorm(normalized_shape, eps, elementwise_affine)
+
+
+def fill_with_neg_inf(t):
+    """FP16-compatible function that fills a input_ids with -inf."""
+    return t.float().fill_(float("-inf")).type_as(t)
